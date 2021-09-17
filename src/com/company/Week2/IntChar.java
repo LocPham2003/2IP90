41a8ad7517cb561a145155f6b9f9405d0d966ee8
Loc, Pham <phamducloc2003@gmail.com>
2021-09-17 23:09:12 +0200

    Made some progress on task 2 algorithm

--- src/com/company/Week2/IntChar.java
+++ src/com/company/Week2/IntChar.java
@@ -1,10 +1,17 @@
 package com.company.Week2;
 
 public class IntChar {
-    public static void main(String[] args) {
-        int a = 1;
 
-        System.out.println(a/2);
+    public static void doSomething(int a) {
+        a++;
+        System.out.println(a);
+    }
+
+    public static void main(String[] args) {
+        int a = 0;
+        doSomething(a);
+
+        System.out.println(a);
     }
 }
 
--- src/com/company/Week2/KingPalindromeList.java
+++ src/com/company/Week2/KingPalindromeList.java
@@ -1,8 +1,6 @@
 package com.company.Week2;
 
-import java.util.Arrays;
 import java.util.Scanner;
-
 
 
 /**
@@ -15,7 +13,7 @@ public class KingPalindromeList {
     /**
      * This function reverse an array for us
      */
-    public static int[] reverse(int a[], int n)
+    public static int[] reverse(int[] a, int n)
     {
         int[] b = new int[n];
         int j = n;
@@ -25,6 +23,17 @@ public class KingPalindromeList {
         }
 
         return b;
+    }
+
+    public static int reconstructNumber(int[] digits, double length) {
+        int number = 0;
+
+        for (int i : digits) {
+            number += i * length;
+            length /= 10;
+        }
+
+        return number;
     }
 
     public int getLengthOfNumber(int a) {
@@ -47,7 +56,50 @@ public class KingPalindromeList {
         return charInNumber;
     }
 
+    public static int[] sortAscending(int[] arr) {
+        int pos;
+        int temp;
+        for (int i = 0; i < arr.length; i++)
+        {
+            pos = i;
+            for (int j = i+1; j < arr.length; j++)
+            {
+                if (arr[j] < arr[pos])                  //find the index of the minimum element
+                {
+                    pos = j;
+                }
+            }
 
+            temp = arr[pos];            //swap the current element with the minimum element
+            arr[pos] = arr[i];
+            arr[i] = temp;
+        }
+
+        return arr;
+    }
+
+    public static int derivePalindrome(int[] biggestPalindrome, int lengthDifference) {
+        // Create another array with same length as the element that is being compared
+        int[] comparable = new int[biggestPalindrome.length - lengthDifference];
+
+        // Check if a palindrome can be derived from the biggest palindrome
+        for (int i = 0; i < lengthDifference/2; i++) {
+            biggestPalindrome[i] = -1;
+            biggestPalindrome[biggestPalindrome.length - (i + 1)] = -1;
+        }
+
+        int index = 0;
+        int otherIndex = 0;
+        while (index < comparable.length) {
+            if (biggestPalindrome[otherIndex] != -1) {
+                comparable[index] = biggestPalindrome[otherIndex];
+                index++;
+            }
+            otherIndex++;
+        }
+
+        return reconstructNumber(comparable, Math.pow(10, comparable.length));
+    }
 
     /**
      * This method will perform task 1 of the assignment
@@ -84,21 +136,16 @@ public class KingPalindromeList {
             }
 
             // Reconstruct the array
-            int palindrome = 0;
             double coefficient = Math.pow(10, length-1);
-            for (int i : charInNumber) {
-                palindrome += i * coefficient;
-                coefficient /= 10;
-            }
 
-            palindromeList[index] = palindrome;
+            palindromeList[index] = reconstructNumber(charInNumber, coefficient);
             index++;
         }
 
         return palindromeList;
     }
 
-    public static void addValueToArray(int[] a, int element, int length) {
+    public static int[] addValueToArray(int[] a, int element, int length) {
             length++;
 
             int[] b = new int[length];
@@ -107,8 +154,9 @@ public class KingPalindromeList {
                 b[i] = a[i];
             }
 
-            b[length] = element;
+            b[length - 1] = element;
 
+            return b;
 
     }
 
@@ -118,6 +166,7 @@ public class KingPalindromeList {
     public void task2(int[] list){
         // Get correct list
         list = task1(list);
+        list = sortAscending(list);
 
         int[][] foundedMiddleValues = new int[10][0];
 
@@ -126,7 +175,33 @@ public class KingPalindromeList {
             int length = getLengthOfNumber(number);
             int[] digits = getDigitsOfNumber(number, length);
 
-            addValueToArray(foundedMiddleValues[digits[length/2]], number, foundedMiddleValues[digits[length/2]].length);
+            foundedMiddleValues[digits[length/2]] = addValueToArray(foundedMiddleValues[digits[length/2]], number, foundedMiddleValues[digits[length/2]].length);
+        }
+
+
+        // Get all sets whose occurrence is more than 1
+        int[] index = new int[0];
+        for (int i = 0; i < foundedMiddleValues.length; i++) {
+            if (foundedMiddleValues[i].length > 1) {
+                index = addValueToArray(index, i, index.length);
+            }
+        }
+
+        // Check if the set is magical
+        for (int i : index) {
+            int longestPalindrome = foundedMiddleValues[i][foundedMiddleValues[i].length - 1];
+            int lengthOfLongestPalindrome = getLengthOfNumber(longestPalindrome);
+            int[] digitsOfLongestPalindrome = getDigitsOfNumber(longestPalindrome, lengthOfLongestPalindrome);
+            for (int k = 1; k < foundedMiddleValues[i].length - 1; k++) {
+                int lengthOfNumber = getLengthOfNumber(foundedMiddleValues[i][k + 1]);
+
+                if (foundedMiddleValues[i][k] != derivePalindrome(digitsOfLongestPalindrome, lengthOfLongestPalindrome - lengthOfNumber)) {
+                    // Remove this value
+                } else {
+                    // Keep it
+                }
+
+            }
         }
     }
 
