615497e4e394bb411bb95f8618855709a257b761
Pham Duc Loc <phamducloc2003@gmail.com>
2021-10-04 16:33:10 +0200

    Finish Madtrucker assignment, all tests passed on Momotor

---
+++ src/com/company/Week4/InfectedCyclist.java
@@ -0,0 +1,13 @@
+package com.company.Week4;
+
+public class InfectedCyclist {
+
+    public void searchGuy() {
+
+    }
+
+    public static void main(String[] args) {
+        String abc = "abc";
+        System.out.println(abc.charAt(0));
+    }
+}
